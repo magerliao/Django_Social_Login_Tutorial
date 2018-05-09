be760a4b8e5da5ff7a76d2e5bd78e14fb194416f
magerliao <lovemyovid@gmail.com>
2018-05-09 16:40:32 +0800

    done

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jessieliao\PycharmProjects\Django_Social_Login_Tutorial\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
